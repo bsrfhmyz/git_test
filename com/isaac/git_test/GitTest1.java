aac01c87df0b131a60b99f98e0e0a05197d8bdf1
icy <mingxuanliu11@gmail.com>
2022-12-06 14:53:56 -0330

    second commit

--- com/isaac/git_test/GitTest1.java
+++ com/isaac/git_test/GitTest1.java
@@ -3,5 +3,6 @@ package com.isaac.git_test;
 public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git 2!");
     }
 }
