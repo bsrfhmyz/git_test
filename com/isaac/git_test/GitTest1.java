108e5b4bc1aec60cdb3a698b87d3d95038093c5d
icy <mingxuanliu11@gmail.com>
2022-12-06 15:45:22 -0330

    hot_fix commit 2

--- com/isaac/git_test/GitTest1.java
+++ com/isaac/git_test/GitTest1.java
@@ -6,5 +6,6 @@ public class GitTest1 {
         System.out.println("hello git 2!");
         System.out.println("hello git 3!");
         System.out.println("hello git 4!");
+        System.out.println("hot_fix test!");
     }
 }
